5b08ad6d4621b5605f07bb257ba146ec1fa2abca
ZZZank <3410764033@qq.com>
2025-08-23 15:35:11 +0800

    split MetadataBuilderJS from MetadataBuilder

--- src/main/java/zank/mods/kube_packages/bridge/kubejs/MetadataBuilderJS.java
+++ src/main/java/zank/mods/kube_packages/bridge/kubejs/MetadataBuilderJS.java
@@ -1,21 +1,51 @@
 package zank.mods.kube_packages.bridge.kubejs;
 
 import com.mojang.serialization.JsonOps;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
 import zank.mods.kube_packages.KubePackages;
-import zank.mods.kube_packages.api.meta.MetadataBuilder;
 import zank.mods.kube_packages.api.meta.PackageMetadata;
+import zank.mods.kube_packages.api.meta.dependency.PackageDependency;
+import zank.mods.kube_packages.impl.dependency.ImmutableMetadata;
 import zank.mods.kube_packages.utils.CodecUtil;
 import zank.mods.kube_packages.utils.GameUtil;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author ZZZank
  */
-public class MetadataBuilderJS extends MetadataBuilder {
+@RequiredArgsConstructor
+@Setter
+@Accessors(fluent = true, chain = true)
+public class MetadataBuilderJS {
+
+    private String id;
+    private String name = null;
+    private String description = null;
+    private ArtifactVersion version = null;
+    private String license = null;
+    private List<String> authors = List.of();
+    private List<PackageDependency> dependencies = List.of();
+
+    public PackageMetadata build() {
+        return new ImmutableMetadata(
+            id,
+            Optional.ofNullable(name),
+            Optional.ofNullable(description),
+            version,
+            Optional.ofNullable(license),
+            authors,
+            dependencies
+        );
+    }
 
     public static final Map<String, PackageMetadata> COMMAND_CACHE = new ConcurrentHashMap<>();
 
