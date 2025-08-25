5d389d61133e297086ffc5fa46c254182bd576e8
ZZZank <3410764033@qq.com>
2025-08-25 20:59:46 +0800

    split "shared data" to another class

--- src/main/java/zank/mods/kube_packages/bridge/kubejs/binding/KubePackagesBinding.java
+++ src/main/java/zank/mods/kube_packages/bridge/kubejs/binding/KubePackagesBinding.java
@@ -26,10 +26,18 @@ public class KubePackagesBinding {
         }
     }
 
-    private final ScriptType type;
+    @Info("""
+        Current script type""")
+    public final ScriptType scriptType;
+    @Info("""
+        Used for storing data meant to be used by other packages, similar to `global`.
+        
+        Users can mutate shared data for current script type and view shared data for all script types""")
+    public final KubePackagesSharedData sharedData;
 
     public KubePackagesBinding(ScriptType type) {
-        this.type = type;
+        this.scriptType = type;
+        this.sharedData = new KubePackagesSharedData(type);
     }
 
     public MetadataBuilderJS metaDataBuilder() {
@@ -45,7 +53,8 @@ public class KubePackagesBinding {
     }
 
     public PackageExporter packageExporter() {
-        return new PackageExporter(message -> this.type.console.info(message.getString()));
+        var console = this.scriptType.console;
+        return new PackageExporter(message -> console.info(message.getString()));
     }
 
     @Info("""
@@ -65,54 +74,5 @@ public class KubePackagesBinding {
         KubePackage id -> KubePackage metadata""")
     public Map<String, PackageMetadata> viewAllMetadata() {
         return Maps.transformValues(KubePackages.getPackages(), KubePackage::metadata);
-    }
-
-    @Info("""
-        Put value into KubePackage shared data for **current** script type
-        
-        @see {@link type} Current script type
-        @see {@link getAllSharedFor} View KubePackage shared data for another script type.""")
-    public void putShared(String id, Object o) {
-        TYPED_GLOBALS.get(type).put(id, o);
-    }
-
-    @Info("""
-        Get KubePackage shared data for **current** script type
-        
-        @see {@link type} Current script type
-        @see {@link getAlSharedFor} View KubePackage shared data for another script type.""")
-    public Object getShared(String id) {
-        return getShared(this.type, id);
-    }
-
-    @Info("""
-        Get KubePackage shared data for specified script type
-        
-        @see {@link type} Current script type
-        @see {@link getAlSharedFor} View KubePackage shared data for another script type.""")
-    public Object getShared(ScriptType type, String id) {
-        return getAllSharedFor(type).get(id);
-    }
-
-    @Info("""
-        View all KubePackage shared data for **current** script type
-        
-        The return value is **immutable**, which means you can't put value into it
-        
-        @see {@link type} Current script type
-        @see {@link getAllSharedFor} View KubePackage shared data for another script type.""")
-    public Map<String, Object> getAllSharedForCurrent() {
-        return getAllSharedFor(type);
-    }
-
-    @Info("""
-        View all KubePackage shared data for specified script type.
-        
-        The return value is **immutable**, which means you can't put value into it
-        
-        @see {@link getAllSharedForCurrent} View all KubePackage shared data for **current** script type
-        @see {@link putShared} Put value into KubePackage shared data for **current** script type""")
-    public Map<String, Object> getAllSharedFor(ScriptType type) {
-        return Collections.unmodifiableMap(TYPED_GLOBALS.get(type));
     }
 }
---
+++ src/main/java/zank/mods/kube_packages/bridge/kubejs/binding/KubePackagesSharedData.java
@@ -0,0 +1,71 @@
+package zank.mods.kube_packages.bridge.kubejs.binding;
+
+import dev.latvian.mods.kubejs.script.ScriptType;
+import dev.latvian.mods.kubejs.typings.Info;
+
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author ZZZank
+ */
+public record KubePackagesSharedData(ScriptType scriptType) {
+    public static final Map<ScriptType, Map<String, Object>> TYPED_GLOBALS = new EnumMap<>(ScriptType.class);
+
+    static {
+        for (var scriptType : ScriptType.values()) {
+            TYPED_GLOBALS.put(scriptType, new ConcurrentHashMap<>());
+        }
+    }
+
+    @Info("""
+        Put value into KubePackage shared data for **current** script type
+        
+        @see {@link type} Current script type
+        @see {@link getAllSharedFor} View KubePackage shared data for another script type.""")
+    public void put(String id, Object o) {
+        TYPED_GLOBALS.get(scriptType).put(id, o);
+    }
+
+    @Info("""
+        Get KubePackage shared data for **current** script type
+        
+        @see {@link type} Current script type
+        @see {@link getAlSharedFor} View KubePackage shared data for another script type.""")
+    public Object get(String id) {
+        return getForType(this.scriptType, id);
+    }
+
+    @Info("""
+        Get KubePackage shared data for specified script type
+        
+        @see {@link type} Current script type
+        @see {@link getAlSharedFor} View KubePackage shared data for another script type.""")
+    public Object getForType(ScriptType type, String id) {
+        return getAllForType(type).get(id);
+    }
+
+    @Info("""
+        View all KubePackage shared data for **current** script type
+        
+        The return value is **immutable**, which means you can't put value into it
+        
+        @see {@link type} Current script type
+        @see {@link getAllSharedFor} View KubePackage shared data for another script type.""")
+    public Map<String, Object> getAll() {
+        return getAllForType(scriptType);
+    }
+
+    @Info("""
+        View all KubePackage shared data for specified script type.
+        
+        The return value is **immutable**, which means you can't put value into it
+        
+        @see {@link getAllSharedForCurrent} View all KubePackage shared data for **current** script type
+        @see {@link putShared} Put value into KubePackage shared data for **current** script type""")
+    public Map<String, Object> getAllForType(ScriptType type) {
+        return Collections.unmodifiableMap(TYPED_GLOBALS.get(type));
+    }
+}
